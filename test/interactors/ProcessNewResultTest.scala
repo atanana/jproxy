8fedbbc6d4a2a71bb547e9551c33be147da381cb
Andrei Tanana <andrei.tanana@deltatre.it>
2018-08-20 14:02:42 +0300

    process new result interactor tests

---
+++ test/interactors/ProcessNewResultTest.scala
@@ -0,0 +1,54 @@
+package interactors
+
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.{FunSuite, OneInstancePerTest}
+import url.UrlTransformer
+import wrappers.{ConfigWrapper, FileSystemWrapper}
+
+class ProcessNewResultTest extends FunSuite with MockFactory with OneInstancePerTest {
+  private val fsWrapper = mock[FileSystemWrapper]
+  private val configWrapper = stub[ConfigWrapper]
+  private val transformer = stub[UrlTransformer]
+  private val interactor = new ProcessNewResult(fsWrapper, configWrapper, transformer)
+
+  private val cacheFile = "cache"
+  private val testUrl = "https://www.nflgamepass.com/api/user/api/users/v1/profile/me"
+  private val testUrlProcessed = "/api/user/api/users/v1/profile/me?host=www.nflgamepass.com&scheme=https"
+
+  transformer.transformToInternalUrl _ when testUrl returns testUrlProcessed
+
+  test("should replace urls") {
+    stubIgnoredPrefixes(List.empty)
+    expectAnyCache
+    val result = interactor.process(cacheFile, s"test $testUrl test")
+    assert(result == s"test $testUrlProcessed test")
+  }
+
+  test("should replace multiple urls") {
+    stubIgnoredPrefixes(List.empty)
+    expectAnyCache
+    val result = interactor.process(cacheFile, s"test $testUrl test $testUrl test")
+    assert(result == s"test $testUrlProcessed test $testUrlProcessed test")
+  }
+
+  test("should not replace ignored urls") {
+    stubIgnoredPrefixes(List("https://www.nflgamepass.com/api/user"))
+    expectAnyCache
+    val result = interactor.process(cacheFile, s"test $testUrl test")
+    assert(result == s"test $testUrl test")
+  }
+
+  test("should store processed result") {
+    stubIgnoredPrefixes(List.empty)
+    fsWrapper.writeFile _ expects(cacheFile, s"test $testUrlProcessed test")
+    interactor.process(cacheFile, s"test $testUrl test")
+  }
+
+  private def stubIgnoredPrefixes(prefixes: Seq[String]): Unit = {
+    configWrapper.ignoredUrlPrefixes _ when() returns prefixes
+  }
+
+  private def expectAnyCache = {
+    fsWrapper.writeFile _ expects(*, *)
+  }
+}
