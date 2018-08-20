11e57ff4655b615f30b9a5a77f65653b187d39c6
Andrei Tanana <andrei.tanana@deltatre.it>
2018-08-20 10:39:54 +0300

    fix index route

--- app/controllers/ProxyController.scala
+++ app/controllers/ProxyController.scala
@@ -2,11 +2,15 @@ package controllers
 
 import interactors.ProxyInteractor
 import javax.inject.{Inject, Singleton}
-import play.api.mvc.{AbstractController, AnyContent, ControllerComponents, Request}
+import play.api.mvc._
 
 @Singleton
 class ProxyController @Inject()(cc: ControllerComponents, proxyInteractor: ProxyInteractor) extends AbstractController(cc) {
   def index(path: String) = Action { implicit request: Request[AnyContent] =>
     Ok(proxyInteractor.processRequest(request.path, request.queryString))
+  }
+
+  def default = Action {
+    Ok("Hello there!")
   }
 }
